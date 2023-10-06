ef5f16ac2058441ba7a0ed4e323349d39e0bff4a
fumekinight <fumeknight_9@proton.me>
2023-10-06 18:42:41 -0400

    feat/responsive design starting files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,244 +8,254 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section
-	class="bg-[#283c4d] min-h-[70vh] text-white flex flex-col gap-6 items-center justify-center text-center px-72"
->
-	<p class="text-5xl">Staffing Innovation Worldwide with Latin America Software Talent.</p>
-
-	<a href="https://calendly.com/fiore-0pe/introductory-meeting-with-stack-it">
-		<button
-			class="text-2xl h-14 text-white rounded-full px-5 bg-[#016fce] outline-none flex items-center justify-center gap-2 w-44"
+<main class="flex flex-col h-full mx-auto">
+	<section class="bg-[#283c4d] w-full">
+		<div
+			class="h-auto w-full max-w-[1200px] mx-auto pt-[10%] px-[15%] pb-[7%] text-white flex flex-col gap-6 items-center justify-center text-center"
 		>
-			Let's Talk
+			<p class="text-5xl">Staffing Innovation Worldwide with Latin America Software Talent.</p>
 
-			<Icon data={arrowRight} />
-		</button>
-	</a>
-</section>
-<!-- Who we are -->
-<section class="bg-white min-h-[70vh] flex py-10 px-28 gap-10">
-	<div class="flex flex-col gap-2">
-		<div class="flex items-center gap-3">
-			<img class="w-12 h-12 object-fit" src="/images/stackit_item.png" alt="" />
-			<h2 class="text-4xl font-bold text-black">Who we are</h2>
+			<a href="https://calendly.com/fiore-0pe/introductory-meeting-with-stack-it">
+				<button
+					class="text-2xl h-14 text-white rounded-full px-5 bg-[#016fce] outline-none flex items-center justify-center gap-2 w-44"
+				>
+					Let's Talk
+
+					<Icon data={arrowRight} />
+				</button>
+			</a>
 		</div>
-		<br />
-		<div class="text-2xl text-blak">
-			<p>
-				At Stack IT, a San Diego, CA-based company, we believe that talent drives success. Since
-				2017, we've connected companies with top tech professionals from Latin America for
-				cost-effective, flexible, and productive remote staffing solutions.
-			</p>
-			<br />
-			<p>
-				Our motto is “Success. Coded.” because our part in your success is to deliver the perfect
-				technical figure you need
-			</p>
+	</section>
+	<!-- Who we are -->
+	<section class="bg-white w-full">
+		<div class="flex flex-col justify-between p-[5%] gap- w-full max-w-[1200px] mx-auto">
+			<div class="flex items-center gap-3 mb-10">
+				<img class="w-12 h-12 object-fit" src="/images/stackit_item.png" alt="" />
+				<h2 class="text-4xl font-bold text-black">Who we are</h2>
+			</div>
+			<div
+				class="flex flex-col justify-between items-center sm:flex-row sm:items-start gap-5 sm:gap-0"
+			>
+				<div class="flex flex-col gap-2 w-full sm:w-3/5">
+					<div class="text-md sm:text-xl lg:text-2xl text-black">
+						<p>
+							At Stack IT, a San Diego, CA-based company, we believe that talent drives success.
+							Since 2017, we've connected companies with top tech professionals from Latin America
+							for cost-effective, flexible, and productive remote staffing solutions.
+						</p>
+						<br />
+						<p>
+							Our motto is “Success. Coded.” because our part in your success is to deliver the
+							perfect technical figure you need
+						</p>
+					</div>
+				</div>
+				<div class="h-full w-[80%] sm:w-2/5">
+					<img alt="who_we_are" class="h-full w-full" src="/images/who_we_are.png" />
+				</div>
+			</div>
 		</div>
-	</div>
-
-	<div>
-		<img src="/images/who_we_are.png" alt="" />
-	</div>
-</section>
-<!-- Staffing Innovation -->
-<section class="bg-white min-h-[70vh] flex py-10 px-28 gap-10">
-	<div>
-		<img src="/images/who_we_are.png" alt="" />
-	</div>
-
-	<div class="flex flex-col gap-2">
-		<div class="flex items-center gap-3">
+	</section>
+	<!-- Staffing Innovation -->
+	<section class="bg-white flex flex-col w-full">
+		<div class="flex items-center gap-3 ml-[calc(5%)] sm:ml-[calc(45%)] sm:w-1/2 w-full">
 			<img class="w-12 h-12 object-fit" src="/images/stackit_item.png" alt="" />
 			<h2 class="text-4xl font-bold text-black">Staffing Innovation</h2>
 		</div>
-		<br />
-		<div class="pr-10 text-2xl text-blak">
-			<p>
-				At Stack IT, a San Diego, CA-based company, we believe that talent drives success. Since
-				2017, we've connected companies with top tech professionals from Latin America for
-				cost-effective, flexible, and productive remote staffing solutions.
-			</p>
-			<br />
-			<p>
-				Our motto is “Success. Coded.” because our part in your success is to deliver the perfect
-				technical figure you need
-			</p>
+		<div
+			class="flex flex-col-reverse sm:flex-row justify-between p-[5%] gap-5 w-full max-w-[1200px] mx-auto"
+		>
+			<div class="w-full sm:w-2/5">
+				<img src="/images/who_we_are.png" alt="" />
+			</div>
+
+			<div class="flex flex-col gap-2 w-full sm:w-3/5 sm:pl-5">
+				<div class="pr-10 text-md sm:text-2xl text-black">
+					<p>
+						At Stack IT, a San Diego, CA-based company, we believe that talent drives success. Since
+						2017, we've connected companies with top tech professionals from Latin America for
+						cost-effective, flexible, and productive remote staffing solutions.
+					</p>
+					<br />
+					<p>
+						Our motto is “Success. Coded.” because our part in your success is to deliver the
+						perfect technical figure you need
+					</p>
+				</div>
+			</div>
 		</div>
-	</div>
-</section>
-<!-- Why we excel. Why us -->
-<section class="bg-[#f2f2f2] min-h-[85vh] flex px-28 gap-10 py-10">
-	<div class="flex flex-col gap-2">
+	</section>
+	<!-- Why we excel. Why us -->
+	<section class="bg-[#f2f2f2] flex w-full">
+		<div class="flex flex-col gap-10 max-w-[1200px] mx-auto p-[5%]">
+			<div class="flex items-center gap-3">
+				<img class="w-12 h-12 object-fit" src="/images/stackit_item.png" alt="" />
+				<h2 class="text-4xl font-bold text-black">Why we excel. Why us.</h2>
+			</div>
+			<div class="text-2xl text-black">
+				<ol class="flex flex-col md:flex-row">
+					<li class="relative flex flex-col w-1/3 gap-16 sm:mb-0">
+						<div class="flex items-center justify-center">
+							<div
+								class="z-10 flex items-center justify-center w-6 h-6 bg-[#016fce] p-7 text-white rounded-full shrink-0"
+							>
+								1
+							</div>
+							<div class="absolute left-44 hidden sm:flex w-full bg-gray-400 h-0.5" />
+						</div>
+						<div class="flex flex-col items-center mt-3">
+							<h3 class="text-3xl font-semibold text-black">Expertise</h3>
+							<p class="font-normal text-center text-black">
+								We leverage our deep knowledge of the LATAM tech industry to connect you with the
+								region's top talent.
+							</p>
+						</div>
+					</li>
+
+					<li class="relative flex flex-col w-1/3 gap-16 sm:mb-0">
+						<div class="flex items-center justify-center">
+							<div
+								class="z-10 flex items-center justify-center w-6 h-6 bg-[#016fce] p-7 text-white rounded-full shrink-0"
+							>
+								2
+							</div>
+							<!-- <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700" /> -->
+						</div>
+						<div class="flex flex-col items-center mt-3">
+							<h3 class="text-3xl font-semibold text-black">Quality</h3>
+							<p class="font-normal text-center text-black">
+								Our veteran tech and HR team upholds rigorous vetting procedures, ensuring each
+								candidate we place is a perfect technical and cultural fit.
+							</p>
+						</div>
+					</li>
+					<li class="relative flex flex-col w-1/3 gap-16 sm:mb-0">
+						<div class="flex items-center justify-center">
+							<div
+								class="z-10 flex items-center justify-center w-6 h-6 bg-[#016fce] p-7 text-white rounded-full shrink-0"
+							>
+								3
+							</div>
+							<div class="absolute right-44 hidden sm:flex w-full bg-gray-400 h-0.5" />
+						</div>
+						<div class="flex flex-col items-center mt-3">
+							<h3 class="text-3xl font-semibold text-black">Consistency</h3>
+							<p class="font-normal text-center text-black">
+								We continually coach our professionals while maintaining close collaboration with
+								your organization. Sustaining the highest quality is our priority.
+							</p>
+						</div>
+					</li>
+				</ol>
+			</div>
+		</div>
+	</section>
+	<!-- How to work together -->
+	<section class="bg-white min-h-[70vh] flex py-10 px-28 gap-10 flex-col">
 		<div class="flex items-center gap-3">
 			<img class="w-12 h-12 object-fit" src="/images/stackit_item.png" alt="" />
-			<h2 class="text-4xl font-bold text-black">Why we excel. Why us.</h2>
+			<h2 class="text-4xl font-bold text-black">How to Work Together</h2>
 		</div>
-		<br />
-		<div class="pr-10 text-2xl text-blak">
-			<ol class="items-center px-20 sm:flex">
-				<li class="relative flex flex-col w-1/3 gap-16 mb-6 h-72 sm:mb-0">
-					<div class="flex items-center justify-center">
-						<div
-							class="z-10 flex items-center justify-center w-6 h-6 bg-[#016fce] p-7 text-white rounded-full shrink-0"
-						>
-							1
-						</div>
-						<div class="absolute left-44 hidden sm:flex w-full bg-gray-400 h-0.5" />
-					</div>
-					<div class="flex flex-col items-center mt-3">
-						<h3 class="text-3xl font-semibold text-black">Expertise</h3>
-						<p class="font-normal text-center text-black">
-							We leverage our deep knowledge of the LATAM tech industry to connect you with the
-							region's top talent.
-						</p>
-					</div>
-				</li>
 
-				<li class="relative flex flex-col w-1/3 gap-16 mb-6 h-72 sm:mb-0">
-					<div class="flex items-center justify-center">
-						<div
-							class="z-10 flex items-center justify-center w-6 h-6 bg-[#016fce] p-7 text-white rounded-full shrink-0"
-						>
-							2
-						</div>
-						<!-- <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700" /> -->
-					</div>
-					<div class="flex flex-col items-center mt-3">
-						<h3 class="text-3xl font-semibold text-black">Quality</h3>
-						<p class="font-normal text-center text-black">
-							Our veteran tech and HR team upholds rigorous vetting procedures, ensuring each
-							candidate we place is a perfect technical and cultural fit.
-						</p>
-					</div>
-				</li>
-				<li class="relative flex flex-col w-1/3 gap-16 mb-6 h-72 sm:mb-0">
-					<div class="flex items-center justify-center">
-						<div
-							class="z-10 flex items-center justify-center w-6 h-6 bg-[#016fce] p-7 text-white rounded-full shrink-0"
-						>
-							3
-						</div>
-						<div class="absolute right-44 hidden sm:flex w-full bg-gray-400 h-0.5" />
-					</div>
-					<div class="flex flex-col items-center mt-3">
-						<h3 class="text-3xl font-semibold text-black">Consistency</h3>
-						<p class="font-normal text-center text-black">
-							We continually coach our professionals while maintaining close collaboration with your
-							organization. Sustaining the highest quality is our priority.
-						</p>
-					</div>
-				</li>
-			</ol>
-		</div>
-	</div>
-</section>
-<!-- How to work together -->
-<section class="bg-white min-h-[70vh] flex py-10 px-28 gap-10 flex-col">
-	<div class="flex items-center gap-3">
-		<img class="w-12 h-12 object-fit" src="/images/stackit_item.png" alt="" />
-		<h2 class="text-4xl font-bold text-black">How to Work Together</h2>
-	</div>
-
-	<div class="flex flex-col w-full gap-5">
-		<!-- Discovery Card -->
-		<div
-			class="self-end flex w-4/5 justify-between text-black border-[2.5px] border-black rounded-3xl items-center h-56 relative"
-		>
-			<div class="flex flex-col items-start h-full pt-5 pl-5 border">
-				<h1 class="font-medium">Discovery</h1>
-				<p class="w-4/5 text-xl">
-					We connect with you to understand your unique project requirements and the specific skills
-					you need.
-				</p>
-			</div>
-			<picture
-				class="h-56 w-56 bg-white m-[-4px] border-[2.5px] border-black rounded-full absolute -right-20 overflow-hidden"
+		<div class="flex flex-col w-full gap-5">
+			<!-- Discovery Card -->
+			<div
+				class="self-end flex w-4/5 justify-between text-black border-[2.5px] border-black rounded-3xl items-center h-56 relative"
 			>
-				<img class="object-contain w-56 h-56 p-5" src="/images/step1.png" alt="Discovery" />
+				<div class="flex flex-col items-start h-full pt-5 pl-5 border">
+					<h1 class="font-medium">Discovery</h1>
+					<p class="w-4/5 text-xl">
+						We connect with you to understand your unique project requirements and the specific
+						skills you need.
+					</p>
+				</div>
+				<picture
+					class="h-56 w-56 bg-white m-[-4px] border-[2.5px] border-black rounded-full absolute -right-20 overflow-hidden"
+				>
+					<img class="object-contain w-56 h-56 p-5" src="/images/step1.png" alt="Discovery" />
+				</picture>
+			</div>
+			<!-- Selection Card -->
+			<div
+				class="self-center flex w-4/5 justify-between text-black border-[2.5px] border-black rounded-3xl items-center h-56 relative"
+			>
+				<div class="flex flex-col items-start h-full pt-5 pl-5 border">
+					<h1 class="font-medium">Selection</h1>
+					<p class="w-4/5 text-xl">
+						We handpick the perfect candidates from our pool of skilled Latin American Developers
+						based on your requirements.
+					</p>
+				</div>
+				<picture
+					class="h-56 w-56 bg-white m-[-4px] border-[2.5px] border-black rounded-full absolute -right-20 overflow-hidden"
+				>
+					<img class="object-contain w-56 h-56 p-5" src="/images/step1.png" alt="Selection" />
+				</picture>
+			</div>
+			<!-- Integration -->
+			<div
+				class=" flex w-4/5 justify-between text-black border-[2.5px] border-black rounded-3xl items-center h-56 relative"
+			>
+				<div class="flex flex-col items-start h-full pt-5 pl-5 border">
+					<h1 class="font-medium">Integration</h1>
+					<p class="w-4/5 text-xl">
+						We ensure seamless integration of the selected developers into your team, driving
+						successful outcomes and bringing value to your projects.
+					</p>
+				</div>
+				<picture
+					class="h-56 w-56 bg-white m-[-4px] border-[2.5px] border-black rounded-full absolute -right-20 overflow-hidden"
+				>
+					<img class="object-contain w-56 h-56 p-5" src="/images/step3.png" alt="Integration" />
+				</picture>
+			</div>
+		</div>
+	</section>
+	<!-- Some of our Clients -->
+	<section class="bg-white min-h-[40vh] flex py-10 px-28 gap-10 flex-col">
+		<div class="flex items-center gap-3">
+			<img class="w-12 h-12 object-fit" src="/images/stackit_item.png" alt="" />
+			<h2 class="text-4xl font-bold text-black">Some of our Clients</h2>
+		</div>
+
+		<div class="flex items-center w-full gap-3">
+			<picture>
+				<img class="w-full h-16" src="/images/fuelzee.png" alt="Fuelzee" />
+			</picture>
+			<picture>
+				<img class="w-full h-28" src="/images/buscape.png" alt="Buscape" />
+			</picture>
+			<picture>
+				<img class="w-full h-16" src="/images/siemens.png" alt="Siemens" />
+			</picture>
+			<picture>
+				<img class="w-full h-16" src="/images/toyota.png" alt="Toyota" />
+			</picture>
+			<picture>
+				<img class="w-full h-16" src="/images/boeing.png" alt="Boeing" />
+			</picture>
+			<picture>
+				<img class="w-full h-16" src="/images/bridgepoint.png" alt="Bridgepoint" />
 			</picture>
 		</div>
-		<!-- Selection Card -->
-		<div
-			class="self-center flex w-4/5 justify-between text-black border-[2.5px] border-black rounded-3xl items-center h-56 relative"
-		>
-			<div class="flex flex-col items-start h-full pt-5 pl-5 border">
-				<h1 class="font-medium">Selection</h1>
-				<p class="w-4/5 text-xl">
-					We handpick the perfect candidates from our pool of skilled Latin American Developers
-					based on your requirements.
-				</p>
-			</div>
-			<picture
-				class="h-56 w-56 bg-white m-[-4px] border-[2.5px] border-black rounded-full absolute -right-20 overflow-hidden"
-			>
-				<img class="object-contain w-56 h-56 p-5" src="/images/step1.png" alt="Selection" />
-			</picture>
+	</section>
+	<!-- The result?  -->
+	<section class="bg-[#f2f2f2] min-h-[30vh] flex py-10 px-28 gap-10 items-center">
+		<div class="flex items-center gap-3">
+			<h2 class="text-4xl font-bold text-black">
+				The result? Top-tier professionals with the skillset to build you nothing but the best
+				software.
+			</h2>
 		</div>
-		<!-- Integration -->
-		<div
-			class=" flex w-4/5 justify-between text-black border-[2.5px] border-black rounded-3xl items-center h-56 relative"
-		>
-			<div class="flex flex-col items-start h-full pt-5 pl-5 border">
-				<h1 class="font-medium">Integration</h1>
-				<p class="w-4/5 text-xl">
-					We ensure seamless integration of the selected developers into your team, driving
-					successful outcomes and bringing value to your projects.
-				</p>
-			</div>
-			<picture
-				class="h-56 w-56 bg-white m-[-4px] border-[2.5px] border-black rounded-full absolute -right-20 overflow-hidden"
+
+		<a href="https://calendly.com/fiore-0pe/introductory-meeting-with-stack-it">
+			<button
+				class="text-2xl h-14 text-white rounded-full px-5 bg-[#016fce] outline-none flex items-center justify-center gap-2 w-44"
 			>
-				<img class="object-contain w-56 h-56 p-5" src="/images/step3.png" alt="Integration" />
-			</picture>
-		</div>
-	</div>
-</section>
-<!-- Some of our Clients -->
-<section class="bg-white min-h-[40vh] flex py-10 px-28 gap-10 flex-col">
-	<div class="flex items-center gap-3">
-		<img class="w-12 h-12 object-fit" src="/images/stackit_item.png" alt="" />
-		<h2 class="text-4xl font-bold text-black">Some of our Clients</h2>
-	</div>
+				Let's Talk
 
-	<div class="flex items-center w-full gap-3">
-		<picture>
-			<img class="w-full h-16" src="/images/fuelzee.png" alt="Fuelzee" />
-		</picture>
-		<picture>
-			<img class="w-full h-28" src="/images/buscape.png" alt="Buscape" />
-		</picture>
-		<picture>
-			<img class="w-full h-16" src="/images/siemens.png" alt="Siemens" />
-		</picture>
-		<picture>
-			<img class="w-full h-16" src="/images/toyota.png" alt="Toyota" />
-		</picture>
-		<picture>
-			<img class="w-full h-16" src="/images/boeing.png" alt="Boeing" />
-		</picture>
-		<picture>
-			<img class="w-full h-16" src="/images/bridgepoint.png" alt="Bridgepoint" />
-		</picture>
-	</div>
-</section>
-<!-- The result?  -->
-<section class="bg-[#f2f2f2] min-h-[30vh] flex py-10 px-28 gap-10 items-center">
-	<div class="flex items-center gap-3">
-		<h2 class="text-4xl font-bold text-black">
-			The result? Top-tier professionals with the skillset to build you nothing but the best
-			software.
-		</h2>
-	</div>
-
-	<a href="https://calendly.com/fiore-0pe/introductory-meeting-with-stack-it">
-		<button
-			class="text-2xl h-14 text-white rounded-full px-5 bg-[#016fce] outline-none flex items-center justify-center gap-2 w-44"
-		>
-			Let's Talk
-
-			<Icon data={arrowRight} />
-		</button>
-	</a>
-</section>
+				<Icon data={arrowRight} />
+			</button>F
+		</a>
+	</section>
+</main>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,26 +2,26 @@
 	import { page } from '$app/stores';
 </script>
 
-<header
-	class="flex justify-between items-center px-28 bg-[#283c4d] transition-all py-2 fixed w-full z-50"
->
-	<div class="">
-		<a href="/">
-			<img src="/images/stackit_logo.png" alt="SvelteKit" class="h-auto w-[220px]" />
-		</a>
-	</div>
+<header class="bg-[#283c4d] fixed w-full z-50">
+	<div class="flex justify-between items-center max-w-[1200px] mx-auto py-[1%] px-[5%]">
+		<div class="">
+			<a href="/">
+				<img src="/images/stackit_logo.png" alt="SvelteKit" class="h-[52px] w-[200px]" />
+			</a>
+		</div>
 
-	<nav class="flex gap-4 text-2xl font-semibold">
-		<a
-			class="text-white hover:underline decoration-blue-500 decoration-4
-			{$page.url.pathname === '/' && 'underline'}"
-			href="/"
-		>
-			Staffing
-		</a>
-		<a class="text-white" href="/custom-software">Custom Software</a>
-		<a href="https://calendly.com/fiore-0pe/introductory-meeting-with-stack-it">
-			<button class="text-white rounded-full px-3 bg-[#016fce] outline-none"> Let's Talk </button>
-		</a>
-	</nav>
+		<nav class="flex gap-4 text-lg">
+			<a
+				class="text-white hover:underline decoration-blue-500 decoration-4
+			{$page.url.pathname === '/' && 'underline font-semibold'}"
+				href="/"
+			>
+				Staffing
+			</a>
+			<a class="text-white" href="/custom-software">Custom Software</a>
+			<a href="https://calendly.com/fiore-0pe/introductory-meeting-with-stack-it">
+				<button class="text-white rounded-full px-3 bg-[#016fce] outline-none"> Let's Talk </button>
+			</a>
+		</nav>
+	</div>
 </header>
