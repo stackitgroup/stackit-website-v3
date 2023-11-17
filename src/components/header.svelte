96d80448bd3ee5516a99eb7d4870cdad8926fe0e
AlefrankM <alefrank.martinez@stackitgroup.com>
2023-11-17 13:05:38 +0000

    Update Stackit logo link in header component

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -7,9 +7,9 @@
 <header
 	class="sticky top-0 z-30 w-full bg-white flex justify-between items-center lg:max-h-[70px] max-w-7xl mx-auto py-2 p-5 2xl:px-0"
 >
-	<picture class="flex items-center w-1/2 gap-2" title="Stackit Logo">
-		<img class="h-12 w-44 object-fit" src="/images/stackit_logo_dark.png" alt="Stakit Isotipo" />
-	</picture>
+	<a href="/" class="flex items-center w-1/2 gap-2" title="Stackit Logo">
+		<img class="h-12 w-44 object-fit" src="/images/stackit_logo_dark.png" alt="Stakit Logotipo" />
+	</a>
 
 	<SideMenu />
 	<Navbar />
