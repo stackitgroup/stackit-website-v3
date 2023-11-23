0266c44ee742a81762e71591be65a17b208a7040
Angel Gonzalez <angel.gonzalez@stackitgroup.com>
2023-11-23 17:46:31 -0400

    adding 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<section class="w-full max-w-[1730px] flex flex-col items-center gap-10">
+	<picture class="h-auto w-[40%]">
+		<img alt="Page Not Found" class="h-full w-full" src="images/404.png" />
+	</picture>
+
+	<a href="/" class="text-sm font-semibold text-[--light-blue]">Go home</a>
+</section>
