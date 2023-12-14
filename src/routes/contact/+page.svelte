1342e53ad7cdf7d4366c8c4d6ab2606dfde155f6
AlefrankM <alefrank.martinez@stackitgroup.com>
2023-12-14 21:03:19 +0000

    Refactor contact page layout and styles

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -10,7 +10,23 @@
 	<title>Contact | Stack IT</title>
 </svelte:head>
 
-<section title="Contact us" class="grid gap-10 gird-cols-1 md:grid-cols-2">
-	<CalendlyWidget />
-	<ContactForm {form} />
+<section title="Contact Us" class="w-full p-[5%] contact-bg">
+	<div class="flex flex-col gap-10 mx-auto w-full max-w-[1730px] text-white">
+		<header class="px-5 2xl:px-0 pb-[5%] flex gap-5 justify-center items-center flex-col">
+			<h2 class="text-2xl">Get in touch</h2>
+			<h3 class="text-5xl text-center">
+				Let's shape the <span class="text-[var(--light-blue)]">future</span>
+			</h3>
+		</header>
+		<div class="grid w-full grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-20">
+			<CalendlyWidget />
+			<ContactForm {form} />
+		</div>
+	</div>
 </section>
+
+<style>
+	.contact-bg {
+		background-image: linear-gradient(180deg, #283c4d -18.66%, #259aff 145.69%);
+	}
+</style>
