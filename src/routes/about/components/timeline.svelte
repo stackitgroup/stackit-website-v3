225435cfde5197d5b3e9ed603aff81060e5bc24b
AlefrankM <alefrank.martinez@stackitgroup.com>
2023-12-15 16:09:53 +0000

    Refactor team member section in timeline.svelte

--- src/routes/about/components/timeline.svelte
+++ src/routes/about/components/timeline.svelte
@@ -16,7 +16,7 @@
 <section id="stack-it-team" class="w-full p-[5%]">
 	<div class="flex flex-col gap-20 w-full max-w-[1730px] mx-auto">
 		<h1 class="text-5xl">Meet Stack <span class="text-[--blue]">IT</span></h1>
-		<section class="grid w-full grid-cols-1 gap-5 pb-20 sm:grid-cols-2 md:grid-cols-3">
+		<section class="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
 			{#each teamMembers as teamMember}
 				<TeamMemberCard {teamMember} {createSelectCaseStudyClick} />
 			{/each}
